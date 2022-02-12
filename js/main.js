5aa4889fe953d711b3168dd529934f2ec7886532
unknown <YunDH218@github.com>
2022-02-12 13:30:59 +0900

    complete

--- js/main.js
+++ js/main.js
@@ -16,19 +16,32 @@ searchInputEl.addEventListener('blur', function() {
 })
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(function () {
   if (window.scrollY > 500) {
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
     })
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
   } else {
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     })
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
   }
 }, 300));
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
@@ -62,6 +75,17 @@ new Swiper('.promotion .swiper', {
   }
 });
 
+new Swiper('.awards .swiper', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+})
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
@@ -92,3 +116,17 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15)
 floatingObject('.floating2', .5, 15)
 floatingObject('.floating3', 1.5, 15)
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+})
+
+const thisYear = document.querySelector('footer .this-year')
+thisYear.textContent = new Date().getFullYear();
